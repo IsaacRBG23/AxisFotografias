d44e236b0e390788a8af8e027e8872cb6b61f42c
Rodrigo <gotzy.irbg@gmail.com>
2024-03-21 12:59:59 -0600

    baches entityes creado con todos los elementos que se necesitan

--- app/src/main/java/com/axis/axisfotografias/data/entityes/bachesEntity.kt
+++ app/src/main/java/com/axis/axisfotografias/data/entityes/bachesEntity.kt
@@ -1,7 +1,24 @@
 package com.axis.axisfotografias.data.entityes
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
-class bachesEntity {
+data class bachesEntity (
+    @PrimaryKey(autoGenerate = true)
+    val idBache : Int? = null,
+    val latitud : Double,
+    val longitud : Double,
+    val minutaAsignada : String,
+    val noBache : Int,
+    val fotoUno : String,
+    val fotoDos : String,
+    val ancho : Double,
+    val Largo : Double,
+    val bOrc : Char,
+    val cuerpo : Char,
+    val carril : Int,
+    val bacheUrgente : Boolean
+
+){
 }
